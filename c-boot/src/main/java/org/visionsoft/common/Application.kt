b15019574f51bbd9900fa5f6fc3eb5f154a0f297
herisn23 <herisn23@gmail.com>
2019-10-04 21:55:26 +0200

    commit pro blade

--- c-boot/src/main/java/org/visionsoft/common/Application.kt
+++ c-boot/src/main/java/org/visionsoft/common/Application.kt
@@ -6,17 +6,20 @@ import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoC
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cglib.core.Converter
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.context.annotation.Import
+import org.springframework.context.support.ConversionServiceFactoryBean
 import org.visionsoft.cmr.mvc.config.MvcConfig
 import org.visionsoft.crm.domain.config.DomainConfig
+import java.util.HashSet
 
 
 @SpringBootApplication//(exclude=[DispatcherServletAutoConfiguration::class])
 @EnableAspectJAutoProxy
-@Import(DomainConfig::class, MvcConfig::class)
+@Import(DomainConfig::class, MvcConfig::class, ConfTest::class, TestConf::class)
 class Application: SpringBootServletInitializer() {
-
     override fun configure(builder: SpringApplicationBuilder) = builder.sources(Application::class.java)!!
 
     companion object {
